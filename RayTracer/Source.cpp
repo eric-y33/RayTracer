27b756738c55f642c1e27cc8700a1821ce014100
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-10-06 11:18:16 -0700

    added rays and a sphere

--- RayTracer/Source.cpp
+++ RayTracer/Source.cpp
@@ -1,14 +1,59 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
+
+// "In graphics, the algebra almost always relates very directly to the geometry."
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
+
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
 	// Image
 
-	int image_width = 256;
-	int image_height = 256;
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
+
+	// Calculate image height, ensure it's at least 1
+	int image_height = int(image_width / aspect_ratio);
+	image_height == (image_height < 1) ? 1 : image_height;
+
+	// Camera
+
+	auto focal_length = 1.0; // distance of viewport from camera
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (double(image_width) / image_height); // Viewport widths <1 are ok since they are real valued
+	auto camera_center = point3(0, 0, 0);
+
+	// Calculate vectors across horizontal and down vertical viewport edges
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	// Calculate horizontal and vertical delta vectors from pixel to pixel
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_height;
+
+	// Calculate location of upper left pixel
+	auto viewport_upper_left = camera_center
+		- vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+	
 
 	// Render
 
@@ -17,7 +62,11 @@ int main() {
 	for (int j = 0; j < image_height; j++) {
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction); // rays shoot out of the camera toward each pixel
+			
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
